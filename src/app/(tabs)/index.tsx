39ccb3323becda8190f2d177f9bd4754a54e976d
Ianduha13 <ianduhamelhayes@gmail.com>
2023-09-09 00:25:42 -0300

    feat: search and favorites screen

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,13 +1,44 @@
-import { FlatList, StyleSheet,Text, View } from 'react-native';
+import { FlatList, StyleSheet,Text, TextInput, View } from 'react-native';
 import { tracks } from '../../../assets/data/tracks';
 import TrackListItem from '../../components/TrackListItem';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { FontAwesome } from '@expo/vector-icons';
+import { useState } from 'react';
 
 export default function HomeScreen() {
+  const [searchText, setSearchText] = useState('');
   return (
+    <SafeAreaView>
+      <View style={styles.header}>
+        <FontAwesome name="search" size={16} color="gray" />
+        <TextInput value={searchText} onChangeText={setSearchText} placeholder="What do you want to listen to?" style={styles.input} />
+        <Text onPress={() => setSearchText('')} style={{color:"white"}}>Cancel</Text>
+      </View>
       <FlatList
       data={tracks}
       renderItem={({item}) => <TrackListItem track={item} />}
       showsVerticalScrollIndicator={false}
       />
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  header:{
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 10,
+  },
+  container:{
+
+  },
+  input:{
+    flex:1,
+    backgroundColor: '#121314',
+    padding: 8,
+    marginHorizontal: 10,
+    color: 'white',
+    borderRadius: 5,
+  }
+
+});
--- src/components/TrackListItem.tsx
+++ src/components/TrackListItem.tsx
@@ -12,7 +12,7 @@ const TrackListItem = ({track} : TrackListItemProps) => {
       <Image
       source={{uri: track.album.images[0]?.url}} style={styles.image}
       />
-      <View style={styles.headerContainer}>
+      <View>
         <Text style={styles.title}>{track.name}</Text>
         <Text style={styles.subtitle}>{track.artists[0]?.name}</Text>
       </View>
@@ -31,8 +31,8 @@ const styles = StyleSheet.create({
     width: '100%',
   },
   title:{
-    color: 'black',
-    ontWeight: '500',
+    color: 'white',
+    fontWeight: '500',
     fontSize: 16,
   }
 ,
